3db77156f59c5b05dcf87645894d150d03341e3d
Beau Hale <rodney.hale@va.gov>
2023-03-14 15:02:58 -0700

    Made Card component.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import ExpenseItem from '../ExpenseItem/ExpenseItem'
+import React from 'react';
+import ExpenseItem from '../ExpenseItem/ExpenseItem';
+import Card from '../Card/Card';
 import './Expenses.css';
 
 export const Expenses = (props) => {
   return (
-    <>
+    <Card className='expenses'>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -25,6 +26,6 @@ export const Expenses = (props) => {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </>
-  )
-}
+    </Card>
+  );
+};
